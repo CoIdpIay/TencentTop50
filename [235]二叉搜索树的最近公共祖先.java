185a5dd259c1e6fc04cbecbdc773414412e65c78
CoIdpIay <56506179+CoIdpIay@users.noreply.github.com>
2021-01-28 00:35:18 +0800

    Add files via upload

---
+++ [235]二叉搜索树的最近公共祖先.java
@@ -0,0 +1,65 @@
+//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。” 
+//
+// 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5] 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+//输出: 6 
+//解释: 节点 2 和节点 8 的最近公共祖先是 6。
+// 
+//
+// 示例 2: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+//输出: 2
+//解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 所有节点的值都是唯一的。 
+// p、q 为不同节点且均存在于给定的二叉搜索树中。 
+// 
+// Related Topics 树 
+// 👍 527 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// 方法1:由二叉排序树的特性可以从根节点找到p,q,并将路径记录在两个数组中,然后遍历两个数组,当第一次出现不同的元素时,上一个元素即为最近的公共祖先
+class Solution {
+    // 方法2:如果遍历二叉排序树,如果当前的结点的值,均大于p,q的值,说明p,q在当前结点的左子树,向左遍历
+    //      如果当前的结点均小于p,q的值,说明p,q在当前结点的右子树,向右遍历
+    //      如果不满足上述两者,则意味着当前结点在p,q的值之间,此时这个结点就是最近的公共祖先或者就是p,q中的某一个值
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
